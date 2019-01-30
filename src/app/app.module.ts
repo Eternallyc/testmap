31641112800f4ddfb8aa9624b7bbec743f4ca21d
Administrator <1306513880@qq.com>
2019-01-30 11:11:24 +0800

    Angular7集成Echart4实现Echarts社区的全国主要城市空气质量实例图表

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {NgxEchartsModule} from 'ngx-echarts';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxEchartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
